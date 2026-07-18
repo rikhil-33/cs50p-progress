8746c4bdb07099c9d6dcf89873bfc3d0a35f5ec1
rikhil-33 <siripurapu.rikhil@gmail.com>
2026-07-18 21:37:13 +0530

    updated

--- week 1: Conditionals/conditionals.py
+++ week 1: Conditionals/conditionals.py
@@ -1,26 +1,23 @@
 def main():
 
     Difficulty = input("Hard or casual").strip().capitalize()
-    players = input("Multiplayer or Single").strip().capitalize()
-
-    if Difficulty == "Hard":
-        if players == "Multiplayer":
-            recommend("Poker")
-        elif Difficulty == "Casual":
-            recommend("Klondike")
-        else:
-            print("Enter a valid num of players")
-   
-    elif Difficulty == "Medium":
-        if players == "Multiplayer":
-            recommend("Hearts")
-        elif Difficulty == "Casual":
-            recommend("Clock")
-        else:
-            print("Enter a valid num of players")
-
-    else: 
+    if not (Difficulty == "Hard" or Difficulty == "Casual"):
         print("Enter a valid difficulty")
+        return
+    
+    players = input("Multiplayer or Single").strip().capitalize()
+    if not (players == "Multiplayer" or players == "Single"):
+        print("Enter a valid num of players")
+        return
+
+    if Difficulty == "Hard" and players == "Multiplayer":
+        recommend("Poker")
+    elif Difficulty == "Hard" and players == "Single":
+        recommend("Klondike")
+    elif Difficulty == "Casual" and players == "Multiplayer":
+        recommend("Hearts")
+    else:
+        recommend("Clock")
 
 def recommend(game):
     print("you might like ", game)
